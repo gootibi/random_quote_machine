8507fe3bb9cfa68cff7022bc96a2f20990ecb674
gootibi <gootibi@gmail.com>
2024-01-05 22:44:36 +0100

    FCC Random Quote project done. Changes to be committed: 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.tsx 	new file:   src/assets/quotes.json 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.tsx 	new file:   src/vite-env.d.ts 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import quotes from './assets/quotes.json';
+import { FaQuoteLeft, FaQuoteRight, FaTwitter, FaTumblr } from "react-icons/fa";
+import './App.css';
+
+interface Quote {
+  quote: string;
+  author: string;
+};
+
+// Quotes function, return random quote and author value
+const getRandomQuote = (): Quote => {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+};
+
+// Random colors (RGB color)
+const getRandomColor = (): string => {
+  const red = Math.floor(Math.random() * 128);
+  const green = Math.floor(Math.random() * 128);
+  const blue = Math.floor(Math.random() * 128);
+
+  return `rgb(${red} ${green} ${blue})`;
+};
+
+const transition = "all 1s";
+
+function App() {
+  // Quotes state
+  const [quote, setQuote] = useState<Quote>(getRandomQuote());
+
+  // Random colors state
+  const [randomColor, setRandomColor] = useState<string>(getRandomColor());
+
+  // OnClick function, set qoute, author, and colors
+  const changeQuote = () => {
+    setQuote(getRandomQuote());
+    setRandomColor(getRandomColor());
+  };
+
+  return (
+    <div className='background' style={{ backgroundColor: randomColor, transition }}>
+      <div id="quote-box">
+        <div className="quote-content" style={{ color: randomColor, transition }}>
+          <h2 id="text">
+            <FaQuoteLeft size="30px" style={{ marginRight: "10px" }} />
+            {quote.quote}
+            <FaQuoteRight size="30px" style={{ marginLeft: "10px" }} />
+          </h2>
+          <h4 id="author">- {quote.author}</h4>
+        </div>
+        <div className="buttons">
+          <a href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote.quote}`}
+            id="tweet-quote"
+            style={{
+              backgroundColor: randomColor,
+              marginLeft: "10px",
+              transition,
+            }}>
+            <FaTwitter color="white" />
+          </a>
+          <button id="new-quote" onClick={changeQuote} style={{ backgroundColor: randomColor, transition }}>
+            Change Quote
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
